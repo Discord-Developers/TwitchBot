a470e899d790f7a522eb74a7fb118e5e602430f7
Nimbi <Darkwater409@gmail.com>
2021-01-21 13:11:42 -0800

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,8 +5,10 @@ module.exports = (client) => {
         .filter(channel => channel.type === 'text')
         .forEach((textChannel) =>
             textChannel.send(stripIndents`
-        **❯ Info**
+        **❯ TwitchBot**
          • Notice: Thank you for inviting me to your guild. Before you can use my commands you must create a few channels. The channels required are listed below.
          • Streaming Channel: 📣｜streaming
+         • ModLog Channel: 💼｜modlog
+         • Welcome Channel: 👋｜welcome
          `).catch(console.error));
 };
