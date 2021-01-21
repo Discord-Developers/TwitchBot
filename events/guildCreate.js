b78030ce81d9a1131d560f575e0bb29ba008f9cf
Nimbi <Darkwater409@gmail.com>
2021-01-20 22:47:38 -0800

    Update code

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -7,9 +7,6 @@ module.exports = (client) => {
             textChannel.send(stripIndents`
         **❯ Info**
          • Notice: Thank you for inviting me to your guild. Before you can use my commands you must create a few channels. The channels required are listed below.
-         • ModLog Channel: 💼｜modlog
-         • Announcements Channel: 📣｜announcements
-         • Broadcasts Channel: 📣｜broadcasts
          • Streaming Channel: 📣｜streaming
          `).catch(console.error));
 };
