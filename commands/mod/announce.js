a317406ea265a1378f99000eeb522ba13443b087
Nimbi <Darkwater409@gmail.com>
2021-01-21 12:43:55 -0800

    Update announce.js

--- commands/mod/announce.js
+++ commands/mod/announce.js
@@ -1,36 +1,29 @@
-const discord = require('discord.js');
 const Commando = require('discord.js-commando');
 
 module.exports = class AnnounceCommand extends Commando.Command {
-  constructor(client) {
-    super(client, {
-      name: 'announce',
-      aliases: ['ann', 'a'],
-      group: 'mod',
-      memberName: 'announce',
-      clientPermissions: ['MANAGE_MESSAGES'],
-      userPermissions: ['MANAGE_MESSAGES'],
-      description: 'Send an announcement to the guild\'s announcements channel',
-      examples: ['ann Hello, world!'],
-      args: [
-        {
-          key: 'text',
-          prompt: 'What would you like the bot to announce?',
-          type: 'string',
-        },
-      ],
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'announce',
+            aliases: ['ann', 'a'],
+            group: 'mod',
+            memberName: 'announce',
+            clientPermissions: ['MANAGE_MESSAGES'],
+            userPermissions: ['MANAGE_MESSAGES'],
+            description: 'Send an announcement to the specified channel',
+            examples: ['ann Hello, world!'],
+            args: [
+                {
+                    key: 'text',
+                    prompt: 'What would you like the bot to announce?',
+                    type: 'string',
+                },
+            ],
+        });
+    }
 
-  run(msg, { text }) {
-    let channel = msg.guild.channels.find(c => c.name === "📣｜announcements")
-    const embed = new discord.RichEmbed()
-      .setAuthor(`TwitchBot`, `https://raw.githubusercontent.com/Discord-Coding-Community/TwitchBot/main/img/avatar.png?size=2048`)
-      .setThumbnail(`https://raw.githubusercontent.com/Discord-Coding-Community/TwitchBot/main/img/avatar.png?size=2048`)
-      .setColor(0xffcc00)
-      .addField(`Announcement`, (text), false)
-      .setFooter(`TwitchBot`, `https://raw.githubusercontent.com/Discord-Coding-Community/TwitchBot/main/img/avatar.png?size=2048`)
-      .setTimestamp();
-    return channel.send(embed);
-  }
+    run(msg, { text }) {
+        let channel = msg.mentions.channels.first();
+        if (!channel) return;
+        return channel.send(text);
+    }
 };
