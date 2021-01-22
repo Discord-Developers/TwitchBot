d6a59d726ec123fff3448f3a9d4015690e234c78
Nimbi <Darkwater409@gmail.com>
2021-01-22 14:24:17 -0700

    Update code

--- events/ready.js
+++
@@ -1,4 +0,0 @@
-module.exports = async (client) => {
-    console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
-    client.user.setActivity(`t!help in ${client.guilds.cache.size} servers`).catch(console.error);
-};
--- index.js
+++ index.js
@@ -1,52 +1,42 @@
-const { client, TwitchChatUser, TwitchChatMessage, CommandoSQLiteProvider } = require('twitch-commando');
+const { TwitchCommandoClient, TwitchChatUser, TwitchChatMessage, CommandoSQLiteProvider } = require('twitch-commando');
 const sqlite = require('sqlite');
 const path = require('path');
-const fs = require('fs');
 require('dotenv').config();
 const token = process.env.CLIENT_TOKEN;
-const prefix = process.env.CLIENT_PREFIX;
 const auth = process.env.OAUTH_TOKEN;
-const owner_id_1 = process.env.OWNER_ID_1;
-const owner_id_2 = process.env.OWNER_ID_2;
+const user = process.env.CLIENT_USERNAME;
+const owner_1 = process.env.OWNER_ID_1;
+const owner_2 = process.env.OWNER_ID_2;
 
 
 
-const client = new TwitchCommandoClient({
-    commandPrefix: prefix,
+var client = new TwitchCommandoClient({
+    username: user,
+    commandPrefix: '!',
     oauth: auth,
     channels: ['#👋｜welcome', '#📣｜streaming', '#💼｜modlog'],
-    botOwners: [owner_id_1, owner_id_2],
-    invite: 'https://dsc.gg/mtdev',
-    disableEveryone: true
+    botOwners: [owner_1, owner_2]
 });
 
+client.enableVerboseLogging();
+
+client.on('connected', () => {    
+});
+
+client.on('join', channel => {
+});
+
+client.on('error', err => {
+});
+
+client.on('message', message => {
+});
 
 client.registerDetaultCommands();
-client.registerCommandsIn(path.join(__dirname, 'commando/twitch'));
-
-
-
-
-const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
-
-for (const file of events) {
-    console.log(`Loading discord.js event ${file}`);
-    const event = require(`./events/${file}`);
-    client.on(file.split(".")[0], event.bind(null, client));
-};
+client.registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.setProvider(
     sqlite.open(path.join(__dirname, 'database.sqlite3')).then(db => new CommandoSQLiteProvider(db))
 );
 
-client.on('connected', () => {
-});
-client.on('join', channel => {
-});
-client.on('error', err => {
-});
-client.on('message', message => {
-});
-
-client.on('error', console.error);
 client.connect(token);
