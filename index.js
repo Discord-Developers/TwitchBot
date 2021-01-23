39d7b3e5e98feda86d21e60571381d4e00aadf95
Nimbi <Darkwater409@gmail.com>
2021-01-22 19:53:57 -0700

    Update Files

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const { TwitchCommandoClient, TwitchChatUser, TwitchChatMessage, CommandoSQLiteProvider } = require('twitch-commando');
-require('enmap');
-const handle = require("discord-error-handler");
-const sqlite = require('sqlite');
+const eslint = require('eslint');
+// const sqlite = require('sqlite');
 const path = require('path');
 require('dotenv').config();
-const fs = require('fs');
+// const fs = require('fs');
 const TOKEN = process.env.CLIENT_TOKEN;
 const env1 = process.env.OWNER_1;
 const env2 = process.env.OWNER_2;
@@ -23,68 +22,69 @@ var client = new TwitchCommandoClient({
 
 client.enableVerboseLogging();
 
-client.on("ready", function(){
+client.on("ready", function () {
     console.log(`the client becomes ready to start`);
-	console.log(`I am ready! Logged in as ${client.user.tag}!`);
-	console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`); 
+    console.log(`I am ready! Logged in as ${client.user.tag}!`);
+    console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
 
-  	client.user.setActivity("the upright organ");
-	client.generateInvite(['SEND_MESSAGES', 'MANAGE_GUILD', 'MENTION_EVERYONE'])
-	.then(link => {
-		console.log(`Generated bot invite link: ${link}`);
-		inviteLink = link;
-	});
+    client.user.setActivity("the upright organ");
+    client.generateInvite(['SEND_MESSAGES', 'MANAGE_GUILD', 'MENTION_EVERYONE'])
+        .then(link => {
+            console.log(`Generated bot invite link: ${link}`);
+            inviteLink = link;
+        });
 });
 
-client.on("reconnecting", function(){
+client.on("reconnecting", function () {
     console.log(`client tries to reconnect to the WebSocket`);
 });
 
-client.on("resume", function(replayed){
+client.on("resume", function (replayed) {
     console.log(`whenever a WebSocket resumes, ${replayed} replays`);
 });
 
-client.on("warn", function(info){
+client.on("warn", function (info) {
     console.log(`warn: ${info}`);
 });
 
-client.on("debug", function(info){
+client.on("debug", function (info) {
     console.log(`debug -> ${info}`);
 });
 
-client.on("error", function(error){
+client.on("error", function (error) {
     console.error(`client's WebSocket encountered a connection error: ${error}`);
 });
 
-client.on("message", function(message){
+client.on("message", function (message) {
     console.log(`message is created -> ${message}`);
 });
 
 
 client.registerDetaultCommands();
 client.registerCommandsIn(path.join(__dirname, 'commands'));
-client.setProvider(
-    sqlite.open(path.join(__dirname, 'database')).then(db => new CommandoSQLiteProvider(db))
-);
+
+
+// client.setProvider(sqlite.open(path.join(__dirname, 'database')).then(db => new CommandoSQLiteProvider(db))
+// );
 
 
 
-fs.readdir('./events/', (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-        const eventFunction = require(`./events/${file}`);
-        if (eventFunction.disabled) return;
-
-        const event = eventFunction.event || file.split('.')[0];
-        const emitter = (typeof eventFunction.emitter === 'string' ? client[eventFunction.emitter] : eventFunction.emitter) || client;
-        const once = eventFunction.once;
-
-        try {
-            emitter[once ? 'once' : 'on'](event, (...args) => eventFunction.run(...args));
-        } catch (error) {
-            console.error(error.stack);
-        }
-    });
-});
+// fs.readdir('./events/', (err, files) => {
+//     if (err) return console.error(err);
+//     files.forEach(file => {
+//         const eventFunction = require(`./events/${file}`);
+//         if (eventFunction.disabled) return;
+// 
+//         const event = eventFunction.event || file.split('.')[0];
+//         const emitter = (typeof eventFunction.emitter === 'string' ? client[eventFunction.emitter] : eventFunction.emitter) || client;
+//         const once = eventFunction.once;
+// 
+//         try {
+//             emitter[once ? 'once' : 'on'](event, (...args) => eventFunction.run(...args));
+//         } catch (error) {
+//             console.error(error.stack);
+//         }
+//     });
+// });
 
 client.connect(TOKEN);
