306afe689048f555e2d6e7d6da951b88c5af5695
Nimbi <Darkwater409@gmail.com>
2021-01-22 18:07:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const OWN2 = process.env.OWNER_ID_2;
 const CH1 = process.env.CHANNEL_1;
 const CH2 = process.env.CHANNEL_2;
 const CH3 = process.env.CHANNEL_3;
+const ERROR_CHANNEL = process.nextTick.CHANNEL_ID;
+const ERROR_GUILD = process.env.GUILD_ID;
 
 var client = new TwitchCommandoClient({
     username: USER,
@@ -26,7 +28,7 @@ var client = new TwitchCommandoClient({
 client.enableVerboseLogging();
 
 client.error = new Map();
-client.logchannel = ["775844088338972693", "802340594504040488"];
+client.logchannel = [ERROR_GUILD, ERROR_CHANNEL];
 client.on('message', async message => {
     try {
         if (message.author.bot) return;
@@ -42,9 +44,9 @@ process.on('unhandledRejection', error => {
 
 client.registerDetaultCommands();
 client.registerCommandsIn(path.join(__dirname, 'commands'));
-// client.setProvider(
-//     sqlite.open(path.join(__dirname, 'database.sqlite3')).then(db => new CommandoSQLiteProvider(db))
-// );
+client.setProvider(
+    sqlite.open(path.join(__dirname, 'database.sqlite3')).then(db => new CommandoSQLiteProvider(db))
+);
 
 
 
