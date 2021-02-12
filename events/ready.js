c01b9f83aa0f6bf62c93d6c9b96f3cda174cd2ee
Nimbi <Darkwater409@gmail.com>
2021-02-12 02:24:04 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,6 +9,6 @@ module.exports = async (client) => {
         console.log(`I am ready! Logged in as ${client.user.tag}!`);
         console.log(`${client.user.tag} has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
 
-        client.user.setActivity(`${ACTIVITY} in ${client.guilds.cache.size} guilds.`);
+        client.user.setActivity(`${ACTIVITY} in ${client.guilds.cache.size} servers`);
 
 };
