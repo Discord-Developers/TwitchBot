a48f5ceb275b1985aeebde506ca1d0dcbe0a8e47
Nimbi <Darkwater409@gmail.com>
2021-02-16 04:16:00 -0700

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,30 +3,16 @@ require('dotenv').config();
 
 const PREFIX = process.env.CLIENT_PREFIX;
 
-module.exports = (client) => {
-
-    client.on("guildCreate", (guild) => {
-        let channelID;
-        let channels = guild.channels.cache;
-
-        channelLoop:
-        for (let key in channels) {
-            let c = channels[key];
-            if (c[1].type === "text") {
-                channelID = c[0];
-                break channelLoop;
-            }
-        }
-
-        let channel = guild.channels.cache.get(guild.systemChannelID || channelID);
-
-        let embed = new Discord.MessageEmbed()
-            .setColor("RANDOM")
-            .setTitle("TwitchBot")
-            .setDesciption(`Thank you for inviting me to your guild.\nType ${PREFIX}help for a list of available commands.`)
-            .setTimestamp()
-        channel.send(embed)
-            .then(console.log)
-            .catch(console.error);
-    })
+module.exports = async function (guild) {
+    const client = this;
+    const channel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
+    console.log(`${client.user.username}` + `has entered` + `${guild.name}.`)
+    let embed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle("TwitchBot")
+        .setDesciption(`Thank you for inviting me to your guild.\nType ${PREFIX}help for a list of available commands.`)
+        .setTimestamp()
+    channel.send(embed)
+        .then(console.log)
+        .catch(console.error);
 };
