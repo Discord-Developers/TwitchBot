b5cb7eab6fadcc962d00e7fc8b508374cedb6a64
Nimbi <Darkwater409@gmail.com>
2021-02-16 04:29:18 -0700

    Update purge.js

--- commands/admin/purge.js
+++ commands/admin/purge.js
@@ -33,7 +33,7 @@ module.exports = class PurgeCommand extends Command {
         const amount = parseInt(args[0]) + 1;
 
         if (isNaN(amount)) {
-            return message.reply('that doesn\'t seem to be a valid number.');
+            return message.reply('```css\n[ERROR] Please provide a valid number.\n```');
         } else if (amount <= 1 || amount > 100) {
             return message.reply('you need to input a number between 1 and 99.');
         }
