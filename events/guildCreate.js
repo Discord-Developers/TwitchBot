ba64a8bde51970807f1297c28b901610c0dc7a2d
Nimbi <Darkwater409@gmail.com>
2021-02-16 17:14:26 -0700

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,15 +4,12 @@ require('dotenv').config();
 const PREFIX = process.env.CLIENT_PREFIX;
 
 module.exports = async function (guild) {
-    const client = this;
-    const channel = guild.channels.cache.find(channel => channel.type === 'text' && channel.permissionsFor(guild.me).has('SEND_MESSAGES'))
-    console.log(`${client.user.username}` + `has entered` + `${guild.name}.`)
-    let embed = new Discord.MessageEmbed()
+    let embed = new Discord.RichEmbed() // Makes a pretty embed
+        .setTitle(client)
+        .setDescription(`Thank you for inviting me to ${guild}`)
         .setColor("RANDOM")
-        .setTitle("TwitchBot")
-        .setDesciption(`Thank you for inviting me to your guild.\nType ${PREFIX}help for a list of available commands.`)
-        .setTimestamp()
-    channel.send(embed)
-        .then(console.log)
-        .catch(console.error);
+        .addField("Support", " - [Discord](https://top.gg/servers/775844088338972693/join)\n - [Website](https://twitchbot.newhorizon.dev)", true)
+        .addField("Info", "Type `t!help` to see a list of my available commands.", true)
+        .setFooter(client.user.avatarUrl, client.user.name);
+    guild.owner.send(embed);
 };
