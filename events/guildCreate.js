55e067c454631ec50c17990c00e9a52902cfcce5
Nimbi <Darkwater409@gmail.com>
2021-02-16 17:34:14 -0700

    Update guildCreate

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,19 +1,20 @@
-const Discord = require('discord.js');
 const stripIndents = require('common-tags').stripIndents;
 require('dotenv').config();
 
 const PREFIX = process.env.CLIENT_PREFIX;
 
 module.exports = async function (guild) {
-    guild.owner.send(stripIndents`
+    client.on("guildMemberAdd", (guild) => {
+        guild.owner.send(stripIndents`
     **❯ TwitchBot**
-     • Thank you for inviting me to ${guild}
+     • Thank you for inviting me to ${guild.name}
 
     **❯ Support**
      • Discord: top.gg/servers/775844088338972693/join
      • Website: twitchbot.newhorizon.dev
 
      **❯ Info**
-     • Type t!help to see a list of available commands.
+     • Type ${PREFIX}help to see a list of available commands.
 `).then(console.log).catch(console.error);
+    })
 };
