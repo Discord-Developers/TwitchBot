211c3428772a03c9f0bec4a39166c5976b0bac6c
Nimbi <Darkwater409@gmail.com>
2021-02-16 17:23:13 -0700

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,15 +1,19 @@
 const Discord = require('discord.js');
+const stripIndents = require('common-tags').stripIndents;
 require('dotenv').config();
 
 const PREFIX = process.env.CLIENT_PREFIX;
 
 module.exports = async function (guild) {
-    let embed = new Discord.RichEmbed() // Makes a pretty embed
-        .setTitle(client)
-        .setDescription(`Thank you for inviting me to ${guild}`)
-        .setColor("RANDOM")
-        .addField("Support", " - [Discord](https://top.gg/servers/775844088338972693/join)\n - [Website](https://twitchbot.newhorizon.dev)", true)
-        .addField("Info", "Type `t!help` to see a list of my available commands.", true)
-        .setFooter(client.user.avatarUrl, client.user.name);
-    guild.owner.send(embed);
+    guild.owner.send(stripIndents`
+    **❯ TwitchBot**
+     • Thank you for inviting me to ${guild}
+
+    **❯ Support**
+     • Discord: top.gg/servers/775844088338972693/join
+     • Website: twitchbot.newhorizon.dev
+
+     **❯ Info**
+     • Type t!help to see a list of available commands.
+`).then(console.log).catch(console.error);
 };
