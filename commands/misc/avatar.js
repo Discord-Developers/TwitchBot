34b0ed58591523351e8ab4d725f8f20f5ea74534
Nimbi <Darkwater409@gmail.com>
2021-02-20 18:52:45 -0700

    Update avatar.js

--- commands/misc/avatar.js
+++ commands/misc/avatar.js
@@ -21,9 +21,11 @@ module.exports = class AvatarCommand extends Command {
 
         let embed = new Discord.MessageEmbed()
             .setTitle(`${user.tag}s profile picture!`)
-            .setURL(user.displayAvatarURL)
             .setImage(user.displayAvatarURL)
             .setColor('RANDOM')
+            .setTimestamp()
+            .setURL(url = 'https://twitchbot.newhorizon.dev/')
+            .setFooter(text = `TwitchBot | twitchbot.newhorizon.dev`, iconUrl = 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
         msg.say(embed)
             .then(console.log)
             .catch(console.error);
