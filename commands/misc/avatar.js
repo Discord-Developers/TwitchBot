443df81c5b2704f99b8f423d8e3194769fd9726c
Nimbi <Darkwater409@gmail.com>
2021-02-21 02:49:44 -0700

    Update avatar.js

--- commands/misc/avatar.js
+++ commands/misc/avatar.js
@@ -4,8 +4,8 @@ const Discord = require('discord.js');
 module.exports = class AvatarCommand extends Command {
     constructor(client) {
         super(client, {
-            name: "avatar",
-            aliases: ["profilepicture"],
+            name: 'avatar',
+            aliases: ['pfp', 'ava'],
             group: 'misc',
             memberName: 'avatar',
             description: 'Sends the avatar of a user.',
@@ -20,13 +20,13 @@ module.exports = class AvatarCommand extends Command {
     run(msg, { user }) {
 
         let embed = new Discord.MessageEmbed()
-            .setTitle(`${user.username}s profile picture!`)
+            .setTitle(`${user.username}s avatar!`)
             .setImage(`${user.avatarURL}`)
             .setColor('RANDOM')
             .setTimestamp()
             .setURL('https://twitchbot.newhorizon.dev/')
-            .setFooter(`TwitchBot | twitchbot.newhorizon.dev`, 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
-        msg.say(embed)
+            .setFooter('TwitchBot | twitchbot.newhorizon.dev', 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
+        msg.reply(embed)
             .then(console.log)
             .catch(err => {
                 console.error(err);
