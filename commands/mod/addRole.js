7004a5c0a744f45acf6438c1d7d29cfb60be9ef0
Nimbi <Darkwater409@gmail.com>
2021-02-20 18:51:37 -0700

    Fix group name typo

--- commands/mod/addRole.js
+++ commands/mod/addRole.js
@@ -4,21 +4,20 @@ const Discord = require('discord.js');
 module.exports = class addRoleCommand extends Command {
     constructor(client) {
         super(client, {
-            name:"addrole",
+            name: "addrole",
             aliases: ["add-role", "arole"],
-            group: 'moderation',
+            group: 'mod',
             memberName: 'addrole',
             description: 'Adds a role to a user.',
-            args: [
-                {
-                    type:"user",
-                    prompt:"Which user would you like to add the role to?",
-                    key:"user",
+            args: [{
+                    type: "user",
+                    prompt: "Which user would you like to add the role to?",
+                    key: "user",
                 },
                 {
-                    type:"role",
-                    prompt:"Which role would you like to add?",
-                    key:"role"
+                    type: "role",
+                    prompt: "Which role would you like to add?",
+                    key: "role"
                 }
             ]
         })
@@ -27,14 +26,14 @@ module.exports = class addRoleCommand extends Command {
 
         msg.guild.member(user).addRole(role)
         let embed = new Discord.MessageEmbed()
-        .setTitle('Role Updated')
-        .setDescription('Successfully added ' + role + ' to ' + user)
-        .setTimestamp()
-        .setColor('RANDOM')
-        .setUrl(url='https://twitchbot.newhorizon.dev/')
-        .setFooter(text=`TwitchBot | twitchbot.newhorizon.dev`, iconUrl='https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
+            .setTitle('Role Updated')
+            .setDescription('Successfully added ' + role + ' to ' + user)
+            .setTimestamp()
+            .setColor('RANDOM')
+            .setUrl(url = 'https://twitchbot.newhorizon.dev/')
+            .setFooter(text = `TwitchBot | twitchbot.newhorizon.dev`, iconUrl = 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
         msg.say(embed)
-        .then(console.log)
-        .catch(console.error);
+            .then(console.log)
+            .catch(console.error);
     }
 };
--- commands/mod/removeRole.js
+++ commands/mod/removeRole.js
@@ -4,21 +4,20 @@ const Discord = require('discord.js');
 module.exports = class removeRoleCommand extends Command {
     constructor(client) {
         super(client, {
-            name:"removerole",
+            name: "removerole",
             aliases: ["remove-role", "rrole"],
-            group: 'moderation',
+            group: 'mod',
             memberName: 'removerole',
             description: 'Removes a role from a user.',
-            args: [
-                {
-                    type:"user",
-                    prompt:"Which user would you like to remove the role from?",
-                    key:"user",
+            args: [{
+                    type: "user",
+                    prompt: "Which user would you like to remove the role from?",
+                    key: "user",
                 },
                 {
-                    type:"role",
-                    prompt:"Which role would you like to remove?",
-                    key:"role"
+                    type: "role",
+                    prompt: "Which role would you like to remove?",
+                    key: "role"
                 }
             ]
         })
@@ -27,14 +26,14 @@ module.exports = class removeRoleCommand extends Command {
 
         msg.guild.member(user).removeRole(role)
         let embed = new Discord.MessageEmbed()
-        .setTitle('Role Updated')
-        .setDescription('Successfully took away the role ' + role + ' from ' + user)
-        .setTimestamp()
-        .setColor('RANDOM')
-        .setUrl(url='https://twitchbot.newhorizon.dev/')
-        .setFooter(text=`TwitchBot | twitchbot.newhorizon.dev`, iconUrl='https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
+            .setTitle('Role Updated')
+            .setDescription('Successfully took away the role ' + role + ' from ' + user)
+            .setTimestamp()
+            .setColor('RANDOM')
+            .setUrl(url = 'https://twitchbot.newhorizon.dev/')
+            .setFooter(text = `TwitchBot | twitchbot.newhorizon.dev`, iconUrl = 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
         msg.say(embed)
-        .then(console.log)
-        .catch(console.error);
+            .then(console.log)
+            .catch(console.error);
     }
 };
