ceb55d01b45d9263549934a0fd7ef7254d93de26
Nimbi <Darkwater409@gmail.com>
2021-02-21 03:58:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// This line simply checks to see if my Node.js version is version 12.
-// If it isn't, it then tells me to update Node.js
 if (Number(process.version.slice(1).split(".")[0]) < 12) throw new Error("Node 12.0.0 or higher is required. Update Node on your system.");
 
 
@@ -14,18 +12,16 @@ require('./.eslintrc.json');
 
 
 
-require('dotenv').config(); // I used dotenv to store my more secure info
-const TOKEN = process.env.CLIENT_TOKEN; // This line reads my Bot Token from dotenv
-const PREFIX = process.env.CLIENT_PREFIX; // This line reads my Client Prefix from dotenv
-const OWNER_ID = process.env.OWNER; // This line reads the bot's owner id from dotenv
+require('dotenv').config();
+const TOKEN = process.env.CLIENT_TOKEN;
+const PREFIX = process.env.CLIENT_PREFIX;
+const OWNER_ID = process.env.OWNER;
 
 
 
-const init = async() => { // I crteated an async wrapper for my client here
+const init = async() => {
 
-
-
-    const client = await new CommandoClient({ // I set my client here
+    const client = await new CommandoClient({
         commandPrefix: PREFIX,
         owner: OWNER_ID,
         disableEveryone: true,
@@ -40,28 +36,28 @@ const init = async() => { // I crteated an async wrapper for my client here
         sqlite.open({ filename: 'database.db', driver: sqlite3.Database }).then(db => new Commando.SQLiteProvider(db))
     ).catch(console.error);
 
-    // Since I'm wrapping my client in an async wrapper, my commands and groups are registered here
-    client.registry
-        // Registers your custom command groups
-        .registerGroups([
-            ['admin', 'Administration'],
-            ['mod', 'Moderation'],
-            ['util', 'Utility'],
-            ['misc', 'Miscellaneous'],
-            ['stream', 'Stream']
-        ])
-        // Registers all built-in groups, commands, and argument types
-        .registerDefaults()
 
-    // Registers all of your commands in the ./commands/ directory
+    client.registry
+
+        .registerGroups([
+        ['admin', 'Administration'],
+        ['mod', 'Moderation'],
+        ['util', 'Utility'],
+        ['misc', 'Miscellaneous'],
+        ['stream', 'Stream']
+    ])
+
+    .registerDefaults()
+
+
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
 
-    client.commands = new Enmap(); // This sets a new Enmap for my commands
-    client.aliases = new Enmap(); // This sets a new Enmap for my command aliases
-    client.settings = new Enmap({ name: "settings" }); //This sets a new Enmap for my settings
+    client.commands = new Enmap();
+    client.aliases = new Enmap();
+    client.settings = new Enmap({ name: "settings" });
 
-    require("./modules/functions.js")(client); // This line makes my bot require the internal functions.js file I have in my bot's modules folder
+    require("./modules/functions.js")(client);
 
     const evtFiles = await readdir("./events/");
     console.log(`Loading a total of ${evtFiles.length} events.`);
@@ -69,13 +65,26 @@ const init = async() => { // I crteated an async wrapper for my client here
         const eventName = file.split(".")[0];
         console.log(`Loading Event: ${eventName}`);
         const event = require(`./events/${file}`);
-        // Bind the client to any event, before the existing arguments
-        // provided by the discord.js event. 
-        client.on(eventName, event.bind(null, client));
 
-        client.login(TOKEN).catch(console.error); // Since my client is in an async wrapper, I imported my TOKEN here
+        client.on(eventName, event.bind(null, client));
+        client.on.client.on("error", (err) => {
+            console.log(`Discord client error '${err.code}' (${err.message})`)
+        });
+        client.on("warn", function(info) {
+            console.log(`warn: ${info}`)
+        });
+        client.on("debug", function(info) {
+            console.log(`debug: ${info}`)
+        });
     });
 
+
+    client.login(TOKEN)
+        .then(console.log)
+        .catch(err => {
+            console.error(err)
+            console.log(`Discord client error '${err.code}' (${err.message})`)
+        });
 };
 
-init(); // Now that I've finished with the rest of my index.js file I have ended the async wrapper here
+init();
