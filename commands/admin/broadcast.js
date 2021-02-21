69d0a4b995addfb3f68b2007e4590b91923b43e4
Nimbi <Darkwater409@gmail.com>
2021-02-20 19:44:38 -0700

    Update broadcast.js

--- commands/admin/broadcast.js
+++ commands/admin/broadcast.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const discord = require('discord.js');
 const { Command } = require('discord.js-commando');
 
 module.exports = class broadcastCommand extends Command {
@@ -16,7 +16,7 @@ module.exports = class broadcastCommand extends Command {
                 duration: 10
             },
             description: 'Sends a global tts broadcast to all text channels in the guild.',
-            examples: ['bc This is a Global TTS Broadcast!'],
+            examples: ['bc TESTING GLOBAL BROADCAST!'],
             args: [{
                 key: 'text',
                 prompt: 'What would you like to broadcast?',
@@ -29,14 +29,7 @@ module.exports = class broadcastCommand extends Command {
         msg.guild.channels.cache
             .filter(channel => channel.type === 'text')
             .forEach((textChannel) => {
-                let embed = new Discord.MessageEmbed()
-                    .setTitle('Broadcast')
-                    .setDescription(`${text}`)
-                    .setURL('https://twitchbot.newhorizon.dev/')
-                    .setFooter('TwitchBot | twitchbot.newhorizon.dev', 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
-                    .setColor('RANDOM')
-                    .setThumbnail('https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
-                textChannel.send(embed, { tts: true })
+                textChannel.send(text, { tts: true })
                     .then(console.log)
                     .catch(err => {
                         console.error(err);
