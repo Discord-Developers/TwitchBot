cdc79d039e28897a1742256949ad7a3a5d1636c9
Nimbi <Darkwater409@gmail.com>
2021-02-20 20:28:02 -0700

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -8,7 +8,7 @@ module.exports = async function(guild) {
     let embed = new DynamicsCompressorNode.MessageEmbed()
         .setTitle('Thanks for Inviting Me')
         .setDescription(`Type ${PREFIX}help to see a list of available commands.`)
-        .addField('Support', '**Discord**: [MountainT Development](https://dsc.gg/mtdev/)\n[Website](https://twitchbot.newhorizon.dev/)', false)
+        .addField('Support', '**Discord**: [MountainT Development](https://dsc.gg/mtdev/)\n**Website**: [TwitchBot](https://twitchbot.newhorizon.dev/)', false)
         .setColor('RANDOM')
         .setFooter('TwitchBot | twitchbot.newhorizon.dev', 'https://images-ext-2.discordapp.net/external/6vZM6YeZGzfxd4PF_aw3UnNHZafkdNlRoLp46YJ7hkU/%3Fsize%3D256/https/cdn.discordapp.com/avatars/779442792324661249/26206ede07f20447bf380df44b429db7.png')
     channel.say(embed)
